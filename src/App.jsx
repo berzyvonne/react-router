368a552768ba9d865f43fbc9cf1480b52931919e
berzyvonne <berzyvonne@gmail.com>
2024-04-17 10:39:47 -0400

    Navar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
+import "./components/Blue"
+import "./components/Red"
+import "./index.css";
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to={"/blue"} element={<h1>Blue</h1>}>Blue</Link>
+          <Link to={"/red"} element={<h1>Red</h1>}>Red</Link>
+          <Link to={"/"} element={<h1>Home</h1>}>Home</Link>
+        </div>
 
         <div id="main-section"> 
         <Routes>
-        <Route path="/blue" element={<h1>Blue</h1>} />
-        <Route path="/red" element={<h1>Red</h1>} />   
+        <Route path={"/blue"} element={<h1>Blue</h1>} />
+        <Route path={"/red"} element={<h1>Red</h1>} />  
+        <Route path={"/"} element={<h1>Home</h1>} />
         </Routes>
         </div>
 
